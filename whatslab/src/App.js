56d0b6726d06dd436994935ff8070176de6bcfdb
Subizbrasil <subizbrasil@gmail.com>
2020-12-18 15:41:21 -0300

    adição CSS e App no projeto

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'app-container'}>
+          <div className={'chat-container'}>
 
-export default App;
+
+
+          </div>
+          <div className={'chat-footer'}>
+            <div id="1">Nome</div>
+            <div id="2">Chat</div>
+            <div id="3">Enviar</div>
+          </div>       
+      </div> 
+    );
+  }
+}
+export default App
